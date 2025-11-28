204530cd40858937ca5be51edbc9140be5be7a74
Pratiksha K Jadhav <72095693+pratikshakj@users.noreply.github.com>
2025-11-28 19:30:47 +0530

    Uncomment area and diameter functions, add array examples
    Uncommented area and diameter calculation functions, and added examples of array manipulations using forEach, map, and filter.

--- javascript.js
+++ javascript.js
@@ -9,24 +9,24 @@ console.log(add(7,3))
 const r =[1,2,3];
 
 
-// //Area
-// function calculateArea(radius){
-//     const result = [];
-//     for(let i = 0;i< radius.length;i++){                                                                                        //         result.push(Math.PI * radius[i] * radius[i])
-//     }
-//     return result;
-// }
-// console.log(calculateArea(radius))
+//Area
+function calculateArea(radius){
+    const result = [];
+    for(let i = 0;i< radius.length;i++){                                                                                        //         result.push(Math.PI * radius[i] * radius[i])
+    }
+    return result;
+}
+console.log(calculateArea(radius))
 
-// // Diameter
-// const calculateDiameter =function (radius){
-//     const res = [];
-//     for (let i = 0; i<radius.length; i++){
-//         res.push(2*radius[i])
-//     }
-//     return res;
-// }
-// // console.log(calculateDiameter(radius))
+// Diameter
+const calculateDiameter =function (radius){
+    const res = [];
+    for (let i = 0; i<radius.length; i++){
+        res.push(2*radius[i])
+    }
+    return res;
+}
+// console.log(calculateDiameter(radius))
 
 //Area logic
 const area = function (r){
@@ -55,9 +55,9 @@ console.log(calculate(r,area));
 console.log(calculate(r,diameter));
 console.log(calculate(r,circumfrance));
 
-//When working with arrays, you can use the map(), reduce(), filter(), and sort() functions to manipulate and transform data in an array.
-//When working with objects, you can use the Object.entries() function to create a new array from an object.
-//When working with functions, you can use the compose() function to create complex functions from simpler ones
+// //When working with arrays, you can use the map(), reduce(), filter(), and sort() functions to manipulate and transform data in an array.
+// //When working with objects, you can use the Object.entries() function to create a new array from an object.
+// //When working with functions, you can use the compose() function to create complex functions from simpler ones
 
 const test = function (){
     console.log('Hello')
@@ -84,18 +84,19 @@ function showRes(sum){ //callback function
     console.log(sum);
 }
 
-adds(7,3, showRes); // this is one form
-adds(7,4, (sum) => console.log(sum)) //this is another form using arrow function
+
+// adds(7,3, showRes); // this is one form
+// adds(7,4, (sum) => console.log(sum)) //this is another form using arrow function
 
 
-//Closures: A closure is a function that has access to its own scope, the outer function's scope, and the global scope.
-let resFunc = adds(6,6,()=>{}) // this will return a function
-resFunc();
+// //Closures: A closure is a function that has access to its own scope, the outer function's scope, and the global scope.
+// let resFunc = adds(6,6,()=>{}) // this will return a function
+// resFunc();
 
 
-Arrays 
+// Arrays 
 
-const  students =['Pratik','Sanket','Ankush','Rohit'];
+// const  students =['Pratik','Sanket','Ankush','Rohit'];
 
 students.push('Ajay'); // adds element at last
 console.log(students + ' '+ students.length );
@@ -104,10 +105,42 @@ console.log(students +' '+ students.length );
 students.push('Ajay');
 console.log(students + students.length );
 
-
 myArr = [true, 'Test', 55] // mixed array
 console.log(myArr);
 myArr.push({state:'KA', Contact: 9988776655}) // adding object to array
 console.log(myArr.indexOf(true)); // gives index of element
 console.log(myArr.includes(56)); // checks if element is present or not
 console.log(myArr.reverse()); // reverses the array
+
+// Arrays in HOF
+const  students =['Pratik','Sanket','Ankush','Rohit'];
+
+function print (n){
+    console.log(n);
+}
+students.forEach(print) // Normal way of calling a function
+students.forEach((val) => console.log(val + "Test")); //Using Arrow function
+
+const numbers =[1,2,3,40,5,6,7]
+let newNums = []             //Ex 1--forEach
+numbers.forEach((nums)=> (newNums.push(nums * 2)))
+console.log(newNums)
+
+function double(a){    //Ex 2 -- map
+    return a*2;
+}
+let newNum= numbers.map(double);
+console.log(newNum)
+
+let ans = numbers.find(num => num === 4)
+console.log(ans)
+
+let arrNum = [1,2,3,4,5,6,88,598,26564]
+arrNum.forEach((val)=>console.log(val**2))
+
+let newMapArr = arrNum.map((val)=>  val*val)
+console.log(newMapArr)
+//filter
+
+let newFilterArr = arrNum.filter((val)=> val > 10000 )
+console.log(newFilterArr)
